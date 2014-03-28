fe84530c07d9a9bedd9af1184681f9dcb7bc5072
iCharge <charge_n@hotmail.com>
2014-03-28 17:13:02 +0700

    add template

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 	// Index file
 	require_once "config.php";
-	include "display.php";
-
-	
+?><!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Test branch</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header"></div>
+		<div class="content"><?php include "display.php"; ?></div>
+		<div class="footer"></div>
+	</div>
+</body>
